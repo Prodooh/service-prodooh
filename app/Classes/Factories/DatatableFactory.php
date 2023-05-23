3b20598e11abbd471b919c19ffea7921ce2a0749
cristoferprodooh <cristoferg@prodooh.com>
2023-05-23 13:46:10 -0600

    [FIX] fix data of datatable

--- app/Classes/Factories/DatatableFactory.php
+++ app/Classes/Factories/DatatableFactory.php
@@ -21,7 +21,7 @@ class  DatatableFactory
     private static function filterUsers()
     {
         if (auth()->user()->hasAnyRole('administrator', 'superadministrator')) {
-            return DB::table('users');
+            return DB::table('users')->join('countries', 'country_id','=','countries.id')->join('companies', 'company_id','=','companies.id')->select('users.id as id','users.name as name','surnames','email','countries.name as country','companies.name as company');
         }
 
         if (auth()->user()->hasRole('salesmanager')) {
