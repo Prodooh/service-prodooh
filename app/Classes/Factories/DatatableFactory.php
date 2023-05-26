0fb70c203f9f176765e576dcfa9d8b741f56db67
cristoferprodooh <cristoferg@prodooh.com>
2023-05-26 10:12:30 -0600

    [FIX] fix changes of the pull request comments

--- app/Classes/Factories/DatatableFactory.php
+++ app/Classes/Factories/DatatableFactory.php
@@ -21,13 +21,21 @@ class  DatatableFactory
     private static function filterUsers()
     {
         if (auth()->user()->hasAnyRole('administrator', 'superadministrator')) {
-            return DB::table('users')->join('countries', 'country_id','=','countries.id')->join('companies', 'company_id','=','companies.id')->select('users.id as id','users.name as name','surnames','email','countries.name as country','companies.name as company');
+            return DB::table('users')->join('countries', 'country_id', '=', 'countries.id')
+                ->join('companies', 'company_id', '=', 'companies.id')
+                ->select('users.id as id', 'users.name as name', 'surnames', 'email', 'countries.name as country', 'companies.name as company');
         }
 
         if (auth()->user()->hasRole('salesmanager')) {
-            return DB::table('users')->where('admin_registry_id', auth()->user()->id);
+            return DB::table('users')->join('countries', 'country_id', '=', 'countries.id')
+                ->join('companies', 'company_id', '=', 'companies.id')
+                ->select('users.id as id', 'users.name as name', 'surnames', 'email', 'countries.name as country', 'companies.name as company')
+                ->where('admin_registry_id', auth()->user()->id);
         }
 
-        return DB::table('users')->where('registry_id', '=', auth()->user()->id);
+        return DB::table('users')->join('countries', 'country_id', '=', 'countries.id')
+            ->join('companies', 'company_id', '=', 'companies.id')
+            ->select('users.id as id', 'users.name as name', 'surnames', 'email', 'countries.name as country', 'companies.name as company')
+            ->where('registry_id', '=', auth()->user()->id);
     }
 }
--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -8,7 +8,10 @@ use App\Http\Requests\UserRequest;
 use App\Models\Company;
 use App\Models\User;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class UserController extends BaseController
 {
@@ -22,6 +25,9 @@ class UserController extends BaseController
     {
         $this->authorize($user);
         $user->update(collect($request->validated())->except('image', 'role')->toArray());
+        if ($request->image) {
+            $this->createImage($request,$user);
+        }
         return $this->successMessage();
     }
 
@@ -47,20 +53,34 @@ class UserController extends BaseController
         return $this->successMessage();
     }
 
+    /**
+     * @param UserRequest $request
+     * @param CreateCompanyAction $action
+     * @return void
+     */
     public function store(UserRequest $request, CreateCompanyAction $action)
     {
         $request['company_id'] = $request->company_id ?? $this->getCompanyByDomain($request, $action);
         $user = User::create($request->except('image', 'role'));
-        if ($request['image']) {
-            $user->image()->create(["url" => $request['image']]);
+        if ($request->image) {
+            $this->createImage($request,$user);
         }
     }
 
+    /**
+     * @param User $user
+     * @return Builder|Model|object|null
+     */
     public function show(User $user)
     {
         return $user->with(['roles:id', 'image'])->first();
     }
 
+    /**
+     * @param $request
+     * @param CreateCompanyAction $action
+     * @return mixed
+     */
     private function getCompanyByDomain($request, CreateCompanyAction $action)
     {
         $domain = explode('@', $request->email)[1];
@@ -70,5 +90,22 @@ class UserController extends BaseController
             return $company[0]['id'];
         }
         return $action->execute(["name" => $nameCompany, "country_id" => $request->country_id])->id;
+    }
+
+    /**
+     * @param Request $request
+     * @param User $user
+     * @return void
+     */
+    private function createImage(Request $request, User $user): void
+    {
+        if ($user->image()->count()) {
+            $user->image()->delete();
+        }
+        if ($request->has('image')) {
+            $user->image()->create([
+                "url" => $request->image
+            ]);
+        }
     }
 }
